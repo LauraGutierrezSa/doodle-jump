8529b78392667e2f548f16e22f859fd1f055a598
Laura Gutierrez <lauragsanahuja@gmail.com>
2024-03-13 15:02:10 +0100

    Make doodle jump up and down

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let isGameOver = false;
   let platformCount = 5;
   let platforms = [];
+  let upTimerId;
+  let downTimerId;
 
   function createDoodler() {
     grid.appendChild(doodler);
@@ -32,15 +34,56 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < platformCount; i++) {
       let platformGap = 600 / platformCount;
       let newPlatformBottom = 100 + i * platformGap;
-      let newPlatform = new Platform();
+      let newPlatform = new Platform(newPlatformBottom);
       platforms.push(newPlatform);
     }
+  }
+
+  function movePlatforms() {
+    if (doodlerBottomSpace > 200) {
+      platforms.forEach((platform) => {
+        platform.bottom -= 4;
+        let visual = platform.visual;
+        visual.style.bottom = platform.bottom + "px";
+      });
+    }
+  }
+
+  function jump() {
+    clearInterval(downTimerId);
+    upTimerId = setInterval(function () {
+      doodlerBottomSpace += 20;
+      doodler.style.bottom = doodlerBottomSpace + "px";
+      if (doodlerBottomSpace > 350) {
+        fall();
+      }
+    }, 30);
+  }
+
+  function fall() {
+    clearInterval(upTimerId);
+    downTimerId = setInterval(function () {
+      doodlerBottomSpace -= 5;
+      doodler.style.bottom = doodlerBottomSpace + "px";
+      if (doodlerBottomSpace <= 0) {
+        gameOver();
+      }
+    }, 30);
+  }
+
+  function gameOver() {
+    console.log("Game over :(");
+    isGameOver = true;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
   }
 
   function start() {
     if (!isGameOver) {
       createDoodler();
       createPlatforms();
+      setInterval(movePlatforms, 30);
+      jump();
     }
   }
 
