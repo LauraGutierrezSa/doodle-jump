48cc12c0a9d238ec22d79c5a8b7e0bdd80f241f0
Laura Gutierrez <lauragsanahuja@gmail.com>
2024-03-13 16:22:08 +0100

    Refactor jump() and fall() functions

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const doodler = document.createElement("div");
   let doodlerLeftSpace = 50;
-  let doodlerBottomSpace = 150;
+  let startPoint = 150;
+  let doodlerBottomSpace = startPoint;
   let isGameOver = false;
   let platformCount = 5;
   let platforms = [];
   let upTimerId;
   let downTimerId;
+  let isJumping = true;
 
   function createDoodler() {
     grid.appendChild(doodler);
     doodler.classList.add("doodler");
+    doodlerLeftSpace = platforms[0].left;
     doodler.style.left = doodlerLeftSpace + "px";
     doodler.style.bottom = doodlerBottomSpace + "px";
   }
@@ -51,10 +54,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function jump() {
     clearInterval(downTimerId);
+    isJumping = true;
     upTimerId = setInterval(function () {
       doodlerBottomSpace += 20;
       doodler.style.bottom = doodlerBottomSpace + "px";
-      if (doodlerBottomSpace > 350) {
+      if (doodlerBottomSpace > startPoint + 200) {
         fall();
       }
     }, 30);
@@ -62,12 +66,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function fall() {
     clearInterval(upTimerId);
+    isJumping = false;
     downTimerId = setInterval(function () {
       doodlerBottomSpace -= 5;
       doodler.style.bottom = doodlerBottomSpace + "px";
       if (doodlerBottomSpace <= 0) {
         gameOver();
       }
+      platforms.forEach((platform) => {
+        if (
+          doodlerBottomSpace >= platform.bottom &&
+          doodlerBottomSpace <= platform.bottom + 15 &&
+          doodlerLeftSpace + 60 >= platform.left &&
+          doodlerLeftSpace <= platform.left + 85 &&
+          !isJumping
+        ) {
+          startPoint = doodlerBottomSpace;
+          jump();
+        }
+      });
     }, 30);
   }
 
@@ -78,10 +95,20 @@ document.addEventListener("DOMContentLoaded", () => {
     clearInterval(downTimerId);
   }
 
+  function control(e) {
+    if (e.key === "ArrowLeft") {
+      //move left
+    } else if (e.key === "ArrowRight") {
+      //move right
+    } else if (e.key === "ArrowUp") {
+      //move straight
+    }
+  }
+
   function start() {
     if (!isGameOver) {
-      createDoodler();
       createPlatforms();
+      createDoodler();
       setInterval(movePlatforms, 30);
       jump();
     }
