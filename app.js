a2ec3d4cd63959a2bfe62f6a6e473c16f07b0f0e
Laura Gutierrez <lauragsanahuja@gmail.com>
2024-03-13 16:39:32 +0100

    Create moveLeft() and moveRight() functions

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
   let platforms = [];
   let upTimerId;
   let downTimerId;
+  let leftTimerId;
+  let rightTimerId;
   let isJumping = true;
+  let isGoingLeft = false;
+  let isGoingRight = false;
 
   function createDoodler() {
     grid.appendChild(doodler);
@@ -97,12 +101,47 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function control(e) {
     if (e.key === "ArrowLeft") {
-      //move left
+      moveLeft();
     } else if (e.key === "ArrowRight") {
-      //move right
+      moveRight();
     } else if (e.key === "ArrowUp") {
-      //move straight
+      moveStraight();
     }
+  }
+
+  function moveLeft() {
+    if (isGoingRight) {
+      clearInterval(rightTimerId);
+      isGoingRight = false;
+    }
+    isGoingLeft = true;
+    leftTimerId = setInterval(function () {
+      if (doodlerLeftSpace >= 0) {
+        doodlerLeftSpace -= 5;
+        doodler.style.left = doodlerLeftSpace + "px";
+      } else moveRight();
+    }, 30);
+  }
+
+  function moveRight() {
+    if (isGoingLeft) {
+      clearInterval(leftTimerId);
+      isGoingLeft = false;
+    }
+    isGoingRight = true;
+    rightTimerId = setInterval(function () {
+      if (doodlerLeftSpace <= 340) {
+        doodlerLeftSpace += 5;
+        doodler.style.left = doodlerLeftSpace + "px";
+      } else moveLeft();
+    }, 30);
+  }
+
+  function moveStraight() {
+    isGoingRight = false;
+    isGoingLeft = false;
+    clearInterval(rightTimerId);
+    clearInterval(leftTimerId);
   }
 
   function start() {
@@ -111,6 +150,7 @@ document.addEventListener("DOMContentLoaded", () => {
       createDoodler();
       setInterval(movePlatforms, 30);
       jump();
+      document.addEventListener("keyup", control);
     }
   }
 
